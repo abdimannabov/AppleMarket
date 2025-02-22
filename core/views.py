a3b62bd543216ef3a3546ddf030166c9a3b73a63
Sohib <abdumanobovsohib13@gmail.com>
2025-02-22 17:48:16 +0500

    added a database and created a welcome page where products are listed

--- core/views.py
+++ core/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from core.models import *
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'core/index.html')
+    items = Product.objects.all()
+    categories = Category.objects.all()
+    return render(request, 'core/index.html',{
+        'categories':categories,
+        'items':items
+    })
 
 def contact(request):
     return render(request, 'core/components/contact.html')
--- market/urls.py
+++ market/urls.py
@@ -1,9 +1,15 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from core.views import *
+from market import settings
 
 urlpatterns = [
     path('', index, name="index"),
     path('contact/', contact, name="contact"),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
